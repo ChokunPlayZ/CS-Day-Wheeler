0d94f396c0ff2f0f5000173e459e5629c9b07b4e
chokunplayz <chokun.swc555@gmail.com>
2022-11-30 09:56:01 +0700

    Fix Forward Reverse Conflict

--- lib/motor.py
+++ lib/motor.py
@@ -2,6 +2,8 @@ import RPi.GPIO as GPIO
 
 def forward(motor):
     GPIO.output(motor['forward'], GPIO.HIGH)
+    GPIO.output(motor['reverse'], GPIO.LOW)
 
 def reverse(motor):
+    GPIO.output(motor['forward'], GPIO.LOW)
     GPIO.output(motor['reverse'], GPIO.HIGH)
