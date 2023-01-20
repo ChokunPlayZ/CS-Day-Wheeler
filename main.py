f6de4e44069b3e18785896166e49249c41e1b183
chokunplayz <chokun.swc555@gmail.com>
2023-01-20 08:04:53 +0700

    moved prod to main.py

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
+import cv2
+import numpy as np
 import RPi.GPIO as gpio
-from flask import Flask, json
+from flask import Flask, jsonify, make_response
 import time
 import lib.motor as motor
-# import lib.mecanum as wheeler
+import lib.mecanum as mecanum
+import subprocess
+import threading
+import picamera
+import io
 
-api = Flask(__name__)
-gpio.cleanup()
+app = Flask(__name__, static_folder='page')
 
-print("""
-CS Day Wheeler V1
-Coded By Yanavut Rojanapron
-https://github.com/chokunplayz
-""")
-
-print("setting up GPIO MODE")
-
-gpio.setwarnings(False)
-
+cap = None
 
 # Define Motors Here
 M1 = {
@@ -39,74 +35,145 @@ M4 = {
     "reverse": 6
 }
 
-try :
+def motor_setup(motor_name):
+    if "forward" in motor_name and "reverse" in motor_name:
+        forward_pin = motor_name["forward"]
+        reverse_pin = motor_name["reverse"]
+        gpio.setup(forward_pin, gpio.OUT)
+        gpio.setup(reverse_pin, gpio.OUT)
+    else:
+        raise ValueError("Error: Forward and reverse pins must be defined for the motor.")
+
+def setup_gpio():
+    gpio.setwarnings(False)
     gpio.setmode(gpio.BCM)
 
-    gpio.setup(17, gpio.OUT)
-    gpio.setup(18, gpio.OUT)
+    motor_setup(M1)
+    motor_setup(M2)
+    motor_setup(M3)
+    motor_setup(M4)
 
-    gpio.setup(27, gpio.OUT)
-    gpio.setup(22, gpio.OUT)
+def create_response(success, code, message):
+    if success:
+        return jsonify({'success': success, 'code': code, 'message': message})
+    else:
+        return jsonify({'success': success, 'error_code': code, 'error_message': message})
 
-    gpio.setup(23, gpio.OUT)
-    gpio.setup(24, gpio.OUT)
-
-    gpio.setup(5, gpio.OUT)
-    gpio.setup(6, gpio.OUT)
-except:
-    print("GPIO SETUP FAILED!, EXITING TO PREVENT FUTURE PROBLEMS")
-    exit()
-
-print("GPIO SETUP COMPLETE")
-
-print("Setting up HTTP WEB SERVICE")
-
-@api.route('/ping', methods=['GET'])
+@app.route('/api/v1/ping', methods=['POST','GET'])
 def ping():
-    return 'Pong'
+    return create_response(True, 200, 'Pong')
 
-@api.route('/forward', methods=['GET'])
-def forward():
-    motor.forward(M1)
-    motor.forward(M2)
-    motor.forward(M3)
-    motor.forward(M4)
-    return 'Command Received'
+@app.route('/api/v1/forward', methods=['POST'])
+def forward_api():
+    mecanum.forward(M1, M2, M3, M4)
+    return create_response(True, 200, 'Command Received')
 
-@api.route('/reverse', methods=['GET'])
-def reverse():
-    motor.reverse(M1)
-    motor.reverse(M2)
-    motor.reverse(M3)
-    motor.reverse(M4)
-    return 'Command Received'
+@app.route('/api/v1/reverse', methods=['POST'])
+def reverse_api():
+    mecanum.reverse(M1, M2, M3, M4)
+    return create_response(True, 200, 'Command Received')
 
-@api.route('/stop', methods=['GET'])
-def stop():
-    motor.stop(M1)
-    motor.stop(M2)
-    motor.stop(M3)
-    motor.stop(M4)
-    return 'Command Received'
+@app.route('/api/v1/stop', methods=['POST'])
+def stop_api():
+    mecanum.stop(M1, M2, M3, M4)
+    return create_response(True, 200, 'Command Received')
 
-@api.route('/move_left', methods=['GET'])
-def move_left():
-    motor.forward(M1)
-    motor.reverse(M2)
-    motor.reverse(M3)
-    motor.forward(M4)
-    return 'Command Received'
+@app.route('/api/v1/move_left', methods=['POST'])
+def move_left_api():
+    mecanum.move_left(M1, M2, M3, M4)
+    return create_response(True, 200, 'Command Received')
 
-@api.route('/move_right', methods=['GET'])
-def move_right():
-    motor.reverse(M1)
-    motor.forward(M2)
-    motor.forward(M3)
-    motor.reverse(M4)
-    return 'Command Received'
+@app.route('/api/v1/move_right', methods=['POST'])
+def move_right_api():
+    mecanum.move_right(M1, M2, M3, M4)
+    return create_response(True, 200, 'Command Received')
 
+@app.route('/api/v1/turn_left', methods=['POST'])
+def turn_left_api():
+    mecanum.turn_left(M1, M2, M3, M4)
+    return create_response(True, 200, 'Command Received')
+
+@app.route('/api/v1/turn_right', methods=['POST'])
+def turn_right_api():
+    mecanum.turn_right(M1, M2, M3, M4)
+    return create_response(True, 200, 'Command Received')
+
+# Global variable to enable/disable auto steer
+auto_steer_enabled = False
+
+@app.route('/still')
+def still():
+    camera = picamera.PiCamera()
+
+    # Set the resolution of the camera
+    camera.resolution = (640, 480)
+
+    # Create a binary stream to store the image data
+    stream = io.BytesIO()
+
+    # Capture a frame and save it to the stream
+    camera.capture(stream, format='jpeg')
+
+    # Reset the stream position to the beginning
+    stream.seek(0)
+
+    # Create a response object with the image data
+    response = make_response(stream.read())
+    response.headers.set('Content-Type', 'image/jpeg')
+
+    # Close the camera
+    camera.close()
+
+    return response
+
+class Camera:
+    def __init__(self):
+        self.camera = picamera.PiCamera()
+        self.camera.resolution = (640, 480)
+        self.camera.framerate = 24
+
+    def get_frame(self):
+        frame = io.BytesIO()
+        self.camera.capture(frame, 'jpeg', use_video_port=True)
+        frame.seek(0)
+        return frame.read()
+
+# API endpoint to enable/disable auto steer
+@app.route('/api/v1/auto_steer', methods=['POST'])
+def auto_steer_api():
+    global auto_steer_enabled
+
+    # Toggle the value of the global variable
+    auto_steer_enabled = not auto_steer_enabled
+
+    if auto_steer_enabled:
+        return create_response(True, 200, 'Auto steer enabled')
+    else:
+        return create_response(True, 200, 'Auto steer disabled')
+
+@app.route('/api/v1/auto_steer/status', methods=['GET'])
+def get_auto_steer_status():
+    global auto_steer_enabled
+    return jsonify({'auto_steer_enabled': auto_steer_enabled})
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+@app.route('/api/v1/shutdown', methods=['POST'])
+def shutdown():
+    shutdown_command = "sudo shutdown -h now"
+    process = subprocess.Popen(shutdown_command.split(), stdout=subprocess.PIPE)
+    output = process.communicate()[0]
+    return create_response(True, 200, output)
+
+# Main function
+def main():
+    # Set up the GPIO pins
+    setup_gpio()
+
+    # Start the Flask API
+    app.run(host='0.0.0.0')
 
 if __name__ == '__main__':
-    api.run(host='0.0.0.0')
-
-print('Finish setting up API Server')
+    main()
